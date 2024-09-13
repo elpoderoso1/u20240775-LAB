3f2221f6172e89d9172ca2ae31ddb5c7dc013c00
Ariel <96531908+elpoderoso1@users.noreply.github.com>
2024-09-13 12:02:31 -0600

    10

--- Práctica 5 G1/Desafio.js
+++ Práctica 5 G1/Desafio.js
@@ -6,15 +6,15 @@ console.log(`Precio del objeto normal $${precio}.`);
 switch (categoria_cliente) {
     case "Básico":
         des = 0.2
-        console.log("Membresía Básico: descuento del 20%.");
+        console.log("Categoria de cliente - Básico: descuento del 20%.");
         break;
     case "Estándar":
         des = 0.4
-        console.log("Membresía Estándar: descuento del 40%.");
+        console.log("Categoria de cliente - Estándar: descuento del 40%.");
         break;
     case "VIP":
         des = 0.6
-        console.log("Membresía VIP: descuento del 60%."); 
+        console.log("Categoria de cliente - VIP: descuento del 60%."); 
         break;
     default:
         console.log("Categoria de cliente no válida.");
