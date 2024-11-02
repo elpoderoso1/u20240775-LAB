4fc620bc904e4f790d50bb1eb431196ace3da22a
Seth <96531908+elpoderoso1@users.noreply.github.com>
2024-11-02 11:59:31 -0600

    10

---
+++ Práctica 12 G1/Ejercicio 1.js
@@ -0,0 +1,41 @@
+class Articulo {
+    constructor(nombre, precio, stock) {
+        this._nombre = nombre;
+        this._precio = precio;
+        this._stock = stock;
+    }
+
+    comprar(cantidad){
+        if (cantidad > 0 && this._stock > 0 && this._stock - cantidad >= 0){
+            this._stock -= cantidad;
+            let total = cantidad * this._precio;
+            if (cantidad > 1){
+                console.log(`Usted ha comprado ${cantidad} unidades del producto ${this._nombre} - Total: $${total}.`);
+            } else {
+                console.log(`Usted ha comprado ${cantidad} unidad del producto ${this._nombre} - Total: $${total}.`);
+            }
+        } else if (this._stock <= 0) {
+            console.log(`El producto ${this._nombre} actualmente esta agotado.`);
+        } else if (this._stock - cantidad < 0){
+            console.log(`La cantidad que quiere comprar es demaciada, actualmente solo hay ${this._stock} unidades del producto ${this._nombre}.`);
+        }
+        else {
+            console.log("La cantidad del producto a comprar debe ser mayor a 0.");
+        }
+    }
+    
+    reabastecer(cantidad){
+        if (cantidad > 0){
+            this._stock += cantidad;
+            console.log(`Reabasteciendo el stock - Cantidad disponible del producto ${this._nombre} = ${this._stock}.`);
+        } else {
+            console.log("La cantidad que desea reabastecer debe ser mayor a 0.");
+        }
+    }
+}
+
+const ART = new Articulo("Jabón", 20, 10);
+ART.comprar(11);
+ART.comprar(1);
+ART.reabastecer(20);
+ART.comprar(15);
--- Práctica 12 G1/Ejercicio 3.js
+++ Práctica 12 G1/Ejercicio 3.js
@@ -4,13 +4,13 @@ class Coche {
         this.modelo = modelo;
     }
     arrancar(){
-        console.log(`${this.marca} ${this.modelo} está arrancando.`);
+        console.log(`${this.marca} ${modelo} está arrancando.`);
     }
-    detener(){
-        console.log(`${this.marca} ${this.modelo} se ha detenido.`);
+    frenar(){
+        console.log(`${this.marca} ${modelo} se ha detenido.`);
     }
 }
 
 const Coche1 = new Coche("Toyota", "Corolla");
 Coche1.arrancar();
-Coche1.detener();
+Coche1.frenar();
